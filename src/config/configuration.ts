dd3ae5a0098390cb073aa4f4f71de06a633dbc4d
nefawaltpint <nefawaltpint@gmail.com>
2022-02-17 10:49:16 -0500

    refactor: cambio dto Object -> string

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,4 +1,4 @@
-/*export const environment =  {
+export const environment =  {
     USER: process.env.CALIFICACIONES_CRUD_USER,
     PASS: process.env.CALIFICACIONES_CRUD_PASS,
     HOST: process.env.CALIFICACIONES_CRUD_HOST,
@@ -6,14 +6,4 @@
     DB: process.env.CALIFICACIONES_CRUD_DB,
     HTTP_PORT: process.env.CALIFICACIONES_CRUD_HTTP_PORT,
     AUTH_DB: process.env.CALIFICACIONES_CRUD_AUTH_DB 
-};*/
-
-export const environment =  {
-    USER: 'consultainv',
-    PASS: '$coninv*',
-    HOST: 'localhost',
-    PORT: '27017',
-    DB: 'bd_registro_notas',
-    HTTP_PORT: '4200',
-    AUTH_DB: 'admin' 
 };
